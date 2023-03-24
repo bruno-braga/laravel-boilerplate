1cf7d4313902982fb19914472dec2814636c12f1
bruno <brunobraga@protonmail.com>
2023-03-23 21:04:22 -0400

    Rebase accept HEAD

--- Modules/User/Exceptions/UserNotFoundException.php
+++ Modules/User/Exceptions/UserNotFoundException.php
@@ -3,14 +3,10 @@
 namespace Modules\User\Exceptions;
 
 use Exception;
-
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-
-class UserNotFoundException extends Exception
+class UserNotFoundException extends \Exception
 {
-    public function render(Request $request): JsonResponse
+    public function render()
     {
-        return response()->json(['status' => 'fail'], 404);
+        return response()->json([], 404);
     }
 }
